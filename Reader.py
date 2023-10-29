faff273815427157f1036610a7113588a4a5dc7b
BuildTools <unconfigured@null.spigotmc.org>
2023-10-29 13:03:15 -0700

    push

--- Reader.py
+++ Reader.py
@@ -1,4 +1,20 @@
 import dpkt
 
-f = open("ass1_1.pcap", 'rb')
+f = open("test.pcap", 'rb')
 pcap = dpkt.pcap.Reader(f)
+
+for timestamp, data in pcap:
+    eth = dpkt.ethernet.Ethernet(data)
+
+    ip = eth.data
+
+    tcp = ip.data
+
+    print(ip.get_proto(ip.p).__name__)
+
+    """ if tcp.dport == 80:
+        try:
+            http = dpkt.http.Request(tcp.data)
+            print(http.headers)
+        except:
+            pass """
