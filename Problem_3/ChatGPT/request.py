8b11d0a27bcab6824a56c6fe240cd8d1d9566431
hjyeung0 <jhyeung@ucdavis.edu>
2023-11-03 11:12:18 -0700

    Updated ChatGPT implementation

--- Problem_3/ChatGPT/request.py
+++ Problem_3/ChatGPT/request.py
@@ -1,18 +1,38 @@
 import requests
 
-print("Hi")
+def create_session():
+    s = requests.Session()
+    s.auth = ('user', 'pass')
+    s.headers.update({'Student-Id': '918630041'})
+    return s
 
-s = requests.Session()
-s.auth = ('user', 'pass')
-s.headers.update({'Student-Id': '918630041'})
+def send_request(session, url, proxies=None, headers=None):
+    try:
+        response = session.get(url, proxies=proxies, headers=headers, verify=False)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        return None
 
-# response with no proxy
-print(("_" * 50) + "\nRESPONSE WITHOUT PROXY", "\n" + ("_" * 50))
-output = requests.get('https://kartik-labeling-cvpr-0ed3099180c2.herokuapp.com/ecs152a_ass1', headers={'Student-Id': '918630041'}, verify=False)
-print(output.headers)
-""" 
-# response with proxy
-print(("_" * 50) + "\nRESPONSE WITH PROXY", "\n" + ("_" * 50))
-proxy = "http://localhost:9000"
-output = requests.get('https://kartik-labeling-cvpr-0ed3099180c2.herokuapp.com/ecs152a_ass1', proxies={'http': proxy, 'https': proxy}, headers={'Student-Id': '918630041'}, verify=False)
-print(output.headers) """
+def main():
+    # Load configuration from a file or environment variables
+    url = 'https://kartik-labeling-cvpr-0ed3099180c2.herokuapp.com/ecs152a_ass1'
+    proxy = "http://localhost:9000"
+    headers = {'Student-Id': '918630041'}
+
+    with create_session() as session:
+        # Make requests with or without a proxy
+        response_without_proxy = send_request(session, url, headers=headers)
+        response_with_proxy = send_request(session, url, proxies={'http': proxy, 'https': proxy}, headers=headers)
+
+    if response_without_proxy:
+        print("_" * 50 + "\nRESPONSE WITHOUT PROXY", "\n" + ("_" * 50))
+        print(response_without_proxy.headers)
+
+    if response_with_proxy:
+        print("_" * 50 + "\nRESPONSE WITH PROXY", "\n" + ("_" * 50))
+        print(response_with_proxy.headers)
+
+if __name__ == "__main__":
+    main()
