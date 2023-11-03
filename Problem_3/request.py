e81ef5878317348c90bd12d6b9dd163f81f4e094
BuildTools <unconfigured@null.spigotmc.org>
2023-11-02 18:08:48 -0700

    proxy

--- Problem_3/request.py
+++ Problem_3/request.py
@@ -7,7 +7,7 @@ s.auth = ('user', 'pass')
 s.headers.update({'Student-Id': '918630041'})
 
 # both 'x-test' and 'x-test2' are sent
-proxy = "http://localhost:8080"
-output = s.get('http://httpbin.org/headers', proxies={'http': proxy, 'https': proxy}, headers={'Student-Id': '918630041'})
+proxy = "http://localhost:9000"
+output = requests.get('https://kartik-labeling-cvpr-0ed3099180c2.herokuapp.com/ecs152a_ass1', proxies={'http': proxy, 'https': proxy}, headers={'Student-Id': '918630041'})
 
 print(output.headers)
