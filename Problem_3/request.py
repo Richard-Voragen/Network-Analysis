07a6edb9124b9d8dbc900314de6b1ed56ede908c
Richard-Voragen <richard24680123@gmail.com>
2023-11-02 17:10:42 -0700

    Corrected student id

--- Problem_3/request.py
+++ Problem_3/request.py
@@ -4,7 +4,7 @@ print("Hi")
 
 s = requests.Session()
 s.auth = ('user', 'pass')
-s.headers.update({'Student-Id': '9186380041'})
+s.headers.update({'Student-Id': '918630041'})
 
 # both 'x-test' and 'x-test2' are sent
-s.get('http://httpbin.org/headers', headers={'Student-Id': '9186380041'})
+s.get('http://httpbin.org/headers', headers={'Student-Id': '918630041'})
